8c451bb348fd3df4b2f502bd57f47afe91cb56eb
ReginaRichie <regina20006@mail.ru>
2022-04-07 22:20:23 +0300

    ыы

---
+++ myworld/chat/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+
+
+def index(request):
+    response = JsonResponse({"login": True, })
+    return response
