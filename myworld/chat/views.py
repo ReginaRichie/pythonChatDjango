86c092df3627db1e356d4f61e139208c13e3d0c7
ReginaRichie <regina20006@mail.ru>
2022-04-17 19:44:33 +0300

    ыы

--- myworld/chat/views.py
+++ myworld/chat/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpRequest
 from django.template import loader
-#from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
-    response: JsonResponse = JsonResponse({"login": True, 'method': request.method})
+    dict_response = {}
+    if request.method == 'POST' and request['login']:
+        dict_response['login'] = request['login']
+
+    response = JsonResponse(dict_response)
     response["Access-Control-Allow-Origin"] = "*"
     response["Access-Control-Allow-Methods"] = "*"
     response["Access-Control-Max-Age"] = "1000"
