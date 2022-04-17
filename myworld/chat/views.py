03848067ab0b23eab9b7bbcace36432b2c2152d5
ReginaRichie <regina20006@mail.ru>
2022-04-17 15:26:03 +0300

    ыы

--- myworld/chat/views.py
+++ myworld/chat/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpRequest
 from django.template import loader
+import requests
 
 
 def index(request):
-    response = JsonResponse({"login": True, })
+    response = JsonResponse({"login": True, 'bla': 'HttpRequest.method'})
     response["Access-Control-Allow-Origin"] = "*"
     response["Access-Control-Allow-Methods"] = "*"
     response["Access-Control-Max-Age"] = "1000"
     response["Access-Control-Allow-Headers"] = "*"
     return response
+
+    # response = requests.get('http://ev-gen.ru/ReginaRichie/')
+    # Вывод кода
+    # print(response.status_code)
+    # Вывод ответа, полученного от сервера API
+    # print(response.json())
