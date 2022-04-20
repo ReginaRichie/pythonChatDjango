af2f05eacc6724b553568d15898c2413d5d460dc
ReginaRichie <regina20006@mail.ru>
2022-04-20 22:37:55 +0300

    some changes

--- myworld/chat/views.py
+++ myworld/chat/views.py
@@ -6,18 +6,13 @@ from .models import Logins
 def index(request):
     dict_response = {}
     mymembers = Logins.objects.all().values()
-    print(mymembers)
-    output = []
-    for x in mymembers:
-        output.append(x["login"])
 
-    print(output)
-    dict_response['test'] = output
-    
-    if request.method == 'POST':
-        if 'login' in request.POST and request.POST['login'] and 'password' in request.POST and request.POST['password']:
-            if request.POST['login'] == 'regina' and request.POST['password'] == '1234':
+    if request.method == 'POST' and 'login' in request.POST and request.POST['login'] and 'password' in request.POST and \
+            request.POST['password']:
+        for mymember in mymembers:
+            if request.POST['login'] == mymember['login'] and request.POST['password'] == mymember['password']:
                 dict_response['login'] = True
+                break
             else:
                 dict_response['login'] = False
 
