a6925979654eb3898c5661010b4dddbb00b3dbd7
ReginaRichie <regina20006@mail.ru>
2022-04-20 22:01:21 +0300

    some changes

--- myworld/chat/models.py
+++ myworld/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Logins(models.Model):
+    objects = None
+    login = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
--- myworld/chat/views.py
+++ myworld/chat/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpRequest
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
-
+from .models import Logins
 
 def index(request):
     dict_response = {}
-
+    mymembers = Logins.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["login"]
+    # print(output)
+    
     if request.method == 'POST':
         if 'login' in request.POST and request.POST['login'] and 'password' in request.POST and request.POST['password']:
             if request.POST['login'] == 'regina' and request.POST['password'] == '1234':
@@ -18,5 +23,5 @@ def index(request):
     response["Access-Control-Allow-Methods"] = "*"
     response["Access-Control-Max-Age"] = "1000"
     response["Access-Control-Allow-Headers"] = "*"
-    return response
-
+    #return response, output
+    return output
